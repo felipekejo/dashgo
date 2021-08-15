508911b348c75d55e1cedade681b80319ac05b90
Felipe Yui <felipe_m87@yahoo.com.br>
2021-08-15 14:50:03 +0930

    Header responsive

--- src/components/Header/Profile.tsx
+++ src/components/Header/Profile.tsx
@@ -1,14 +1,20 @@
 import { Avatar, Box, Flex, Text } from "@chakra-ui/react";
 
-export function Profile() {
+interface ProfileProps {
+  showProfileData: boolean
+}
+
+export function Profile({ showProfileData = true }: ProfileProps) {
   return (
     <Flex align="center">
-      <Box mr="4" textAlign="right">
-        <Text>Felipe Yui</Text>
-        <Text color="gray.300" fontSize="small">
-          felipe_m87@yahoo.com.br
-        </Text>
-      </Box>
+      {showProfileData && (
+        <Box mr="4" textAlign="right">
+          <Text>Felipe Yui</Text>
+          <Text color="gray.300" fontSize="small">
+            felipe_m87@yahoo.com.br
+          </Text>
+        </Box>
+      )}
       <Avatar size="md" name="Felipe Yui" src="/felipe.jfif" />
 
     </Flex>
