f002cbad80e33d754863360f7d9e3aef52cd9207
Felipe Yui <felipe_m87@yahoo.com.br>
2021-12-28 15:54:09 +1030

    logout

--- src/contexts/AuthContext.tsx
+++ src/contexts/AuthContext.tsx
@@ -1,6 +1,6 @@
 import Router from "next/router";
 import { createContext, ReactNode, useEffect, useState } from "react";
-import { setCookie, parseCookies } from 'nookies'
+import { setCookie, parseCookies, destroyCookie } from 'nookies'
 
 import { api } from "../services/api";
 
@@ -27,6 +27,13 @@ type AuthProviderProps = {
 
 export const AuthContext = createContext({} as AuthContextData)
 
+export function signOut() {
+  destroyCookie(undefined, 'dashgo.token')
+  destroyCookie(undefined, 'dashgo.refreshToken')
+
+  Router.push('/')
+}
+
 export function AuthProvider({ children }: AuthProviderProps) {
   const [user, setUser] = useState<User>(null)
   const isAuthenticated = !!user;
@@ -39,6 +46,9 @@ export function AuthProvider({ children }: AuthProviderProps) {
         const { email, permission, roles } = response.data
         setUser({ email, permission, roles })
       })
+        .catch(() => {
+          signOut()
+        })
     }
   }, [])
 
